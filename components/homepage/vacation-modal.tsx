bf65948aacc5d87b0d16b474ddcf6f8eec27d82e
arisas <arisashiraishi@outlook.com>
2025-03-19 12:42:04 +0900

    add overlay

--- components/homepage/vacation-modal.tsx
+++ components/homepage/vacation-modal.tsx
@@ -45,7 +45,7 @@ export default function VacationModal({
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed inset-0 z-50 flex items-center justify-center px-4"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 px-4"
           onClick={handleClose}
         >
           <motion.div
